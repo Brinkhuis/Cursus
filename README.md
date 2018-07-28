a78e50f42591644a25d6c4919f31f8f5df7d3b6b
Brinkhuis <rene.brinkhuis@gmail.com>
2018-07-28 15:59:30 +0200

    update

--- README.md
+++ README.md
@@ -4,15 +4,14 @@ Cursus Python
 To Do List
 
 * notebook met alle mogelijkheden van het notebook maken, incl. help / shortscuts, html, markdown, plaatjes, font colors en ook de notebook viewer online via github voor het delen van notebooks.
-* script om file te creeeren
+v script om file te creeeren
 * check documentatie cursussen DataCamp i.v.m. voorkomen overlap (wel "opfrisser")
-* maak separaat nootbook met uitwerkingen van de opdrachten (of verberg deze cellen)
+v maak separaat nootbook met uitwerkingen van de opdrachten (of verberg deze cellen)
 * maak een index met in welke volgorde wat te doen tijdens de cursus
 * in cursus (datatypes) aangeven wanneer welke opgave gemaakt moet worden.
 * ...
-* Bokeh, Folium, Plotly, Matplotlib, pandas plot, Seaborn
-* map function! with lambda! h_letters = list(map(lambda x: x, 'human'))
+v Bokeh, Folium, Plotly, Matplotlib, pandas plot, Seaborn
+v map function! with lambda! h_letters = list(map(lambda x: x, 'human'))
 * ...
-* Notebook Viewer online vs Github (rendering)
-
-
+v Notebook Viewer online vs Github (rendering)
+* Opgave widgets en Bokeh toevoegen
---
+++ scripts/bokeh_app.py
@@ -0,0 +1,37 @@
+# imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure, output_file, show, reset_output
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import widgetbox, row, column
+from bokeh.models.widgets import Select, Dropdown
+from bokeh.sampledata.iris import flowers # load data
+
+# setup data
+species = 'versicolor'
+source = ColumnDataSource(flowers.loc[flowers.species == species])
+source.data = ColumnDataSource.from_df(flowers.loc[flowers.species == species])
+
+# setup tooltips
+tooltips = [('index', '@index'),
+            ('Sepal Length', '@sepal_length'),
+            ('Sepal Width', '@sepal_width'),
+            ('Species', '@species')]
+
+# setup plot
+
+p = figure(plot_width = 400, plot_height = 400, tooltips = tooltips)
+p.circle('sepal_length', 'sepal_width', size = 10, color = 'blue', alpha = 0.5, source = source)
+
+# setup widget
+menu = [(s.capitalize(), s) for s in flowers.species.unique()]
+dropdown = Dropdown(label = 'Species',  menu = menu)
+
+# callback to widgets
+def callback(attr, old, new):
+    species = dropdown.value
+    source.data = ColumnDataSource.from_df(flowers.loc[flowers.species == species])
+dropdown.on_change('value', callback)
+
+# arrange plots and widgets in layouts
+layout = column(dropdown, p)
+curdoc().add_root(layout)
